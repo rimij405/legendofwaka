35674f2186b3f4e36b20d3b6178483f91101a2a1
JohnVance1 <33812147+JohnVance1@users.noreply.github.com>
2018-01-28 00:45:27 -0500

    Created Classes
    Created Entity Class, Player Class, Health Class, and made the Cub Prefab.

---
+++ LOW/LegendOfWaka/Assets/Scripts/Entity.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour {
+
+    private Vector2 startingLocation;
+    private Health health;
+    private bool isVisible;
+    
+    public Vector2 StartingLocation
+    {
+        get { return startingLocation; }
+        set { startingLocation = value; }
+
+    }
+
+    public Health Health
+    {
+        get { return health; }
+
+    }
+
+    public bool IsAlive
+    {
+        get { return Health.IsAlive(); }
+
+    }
+
+    public bool IsVisible
+    {
+        get { return isVisible; }
+        set { isVisible = value; }
+
+    }
+
+    public string Name
+    {
+        get { return gameObject.name; }
+        set { gameObject.name = value; }
+
+    }
+
+   
+	// Use this for initialization
+	void Start ()
+    {
+        health = gameObject.AddComponent<Health>();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(isVisible == true)
+        {
+            gameObject.GetComponent<MeshRenderer>().enabled = true;
+
+        }
+
+        else
+        {
+            gameObject.GetComponent<MeshRenderer>().enabled = false;
+            
+        }
+
+        Debug.Log("Entity Health Component: " + Health.IsAlive() + " " + Health.Value);
+
+    }
+}
---
+++ LOW/LegendOfWaka/Assets/Scripts/Health.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+    private int value;
+    private int maxHealth;
+    private int minHealth;
+
+    public int Value
+    {
+        get { return value; }
+
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+        Restore();
+
+
+	}
+
+    public void Restore()
+    {
+        Heal(maxHealth);
+
+    }
+
+    public void Heal(int health)
+    {
+        value += Mathf.Abs(health);
+
+        if(value > maxHealth)
+        {
+            value = maxHealth;
+
+        }
+
+    }
+
+    public void Damage(int damage)
+    {
+        value -= damage;
+
+        if(value < minHealth)
+        {
+            value = minHealth;
+
+        }
+
+    }
+
+    public bool IsAlive()
+    {
+        return (value > minHealth) ;
+       
+    }
+
+    public bool FullHealth()
+    {
+        return (value == maxHealth);
+
+    }
+
+    public void Kill()
+    {
+        Damage(maxHealth);
+
+    }
+
+
+	
+	
+
+
+}
